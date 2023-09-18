d63ceeb7bb006d95aea90d8b26b90e7bff1f70f9
zhibekzhaan <kyzzhibek0908@gmail.com>
2023-09-18 21:41:51 +0600

    Initial Commit

---
+++ Notebook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notebook
+//
+//  Created by zhibekzhaan on 9/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
